2e1a223904bd35a6980dc791908de60bf165e090
Napstar <napoleon.dev1@gmail.com>
2019-02-02 16:30:24 -0400

    add googlemaps

--- www/js/angular/app.js
+++ www/js/angular/app.js
@@ -16,9 +16,102 @@ oppoKnoocksApp.controller('BackGeoController', function BackGeoController($scope
   $scope.message ="Back Geo";
 
 });
-oppoKnoocksApp.controller('GoogleMapsController', function GoogleMapsController($scope,$log,$q) {
+oppoKnoocksApp.controller('GoogleMapsController', function GoogleMapsController($scope,$log,$q,GoogleMapsFactory) {
   $scope.message ="Google Maps";
+  // $scope.getGoogleMap=function(){
+  //   var promise =GoogleMapsFactory.getGoogleMap();
+  //   promise.then(
+  //      function(payload)
+  //      {
+  //       $scope.mapData = payload.data;
+  //      },
+  //      function(err)
+  //      {
+  //      console.log('error in loading maps', err);
+  //      }
+  //   );
 
+    
+  // };
+  $scope.getGoogleMap2=function(){
+  //  let   cordova= {};
+     const apiKEY="AIzaSyA5Ti9F4VNAoFtRynoHPsbxiatN_JTY9tE";
+     //let url="https://maps.googleapis.com/maps/api/js?key="+apiKEY+"&sensor=true";
+    // let params = {};
+    // let headers = {};
+    // cordova.plugin.http.get(url, 
+    //     params, headers, (response) => {
+    //   console.log(response.status);
+    // }, function(response) {
+    //   console.error(response.error);
+    // });
+  //   let headers = new Headers();
+
+  // headers.append('Content-Type', 'application/json');
+  // headers.append('Accept', 'application/json');
+
+  // headers.append('Access-Control-Allow-Origin', '*');
+  // headers.append('Origin','http://localhost:8000');
+  //   var request = new Request(url);
+
+  //   fetch(request, {mode: 'no-cors'}).then(function(response) {
+  //       // Convert to JSON
+  //       alert("success");
+  //       console.log(response);
+  //       return response.json();
+  //   }).then(function(j) {
+  //       // Yay, `j` is a JavaScript object
+  //       console.log(j);
+  //       console.log(JSON.stringify(j));
+  //   }).catch(function(error) {
+  //     alert(error);
+  //       console.log('Request failed', error)
+  //   });
+  var googleMapOption = {
+    zoom: 4,
+    center: new google.maps.LatLng(25, 80),
+    mapTypeId: google.maps.MapTypeId.TERRAIN
+  };
+
+  $scope.gMap = new google.maps.Map(document.getElementById('map'), googleMapOption);
+    
+   };
+  
+
+  var init = function () {
+    var map;
+
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+    
+    $scope.loadScript = function() {
+      var script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyA5Ti9F4VNAoFtRynoHPsbxiatN_JTY9tE&callback=init';
+      document.body.appendChild(script);
+      setTimeout(function() {
+          $scope.initialize();
+      }, 500);
+  }
+    // var promise =GoogleMapsFactory.getGoogleMap();
+    // promise.then(
+    //    function(payload)
+    //    {
+    //     console.log('Success in loading maps', payload.data);
+    //     $scope.mapData = payload.data;
+    //    },
+    //    function(err)
+    //    {
+    //    console.log('error in loading maps', err);
+    //    alert(err);
+    //    }
+    // )
+    // .then();
+ };
+  
+ init();
 });
 oppoKnoocksApp.controller('DeviceGalleryController', function DeviceGalleryController($scope,$log,$q) {
   $scope.message ="DeviceGallery";
@@ -33,7 +126,20 @@ oppoKnoocksApp.controller('FileTransferController', function FileTransferControl
 
 });
 
+oppoKnoocksApp.factory("GoogleMapsFactory",function($http,$q){
+  let deferred = $q.defer();
+  const apiKEY="AIzaSyA5Ti9F4VNAoFtRynoHPsbxiatN_JTY9tE";
+  let strAPI="https://maps.googleapis.com/maps/api/js?key="+apiKEY+"&sensor=true";
+  return{
+    getGoogleMap:function(){
+      return $http.get(strAPI);
+    },
+    getGoogleMap2:function(){
+      return $http.get(strAPI);
+    }
+  }
 
+});
 
 oppoKnoocksApp.factory('CameraFactory', function ($q) {
   var deferred = $q.defer();
@@ -46,14 +152,19 @@ oppoKnoocksApp.factory('CameraFactory', function ($q) {
   };
   return {
     takePicture: function () {
+      let $image = $('#photo');
       navigator.camera.getPicture(function (imageURI) {
 
-        var image = $('#photo');
+    
+        console.log(imageURI);
+        $image.show();
         image.attr('src', imageURI);
         
 
       }, function (errorMessage) {
-        alert('The following error occured: ' + errorMessage)
+        
+        alert('The following error occured: ' + errorMessage);
+        deferred.reject(error);
       }, cameraOptions);
       return deferred.promise;
     }
